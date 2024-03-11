940aa12555c90db284c62d4a2a106a5b28e15589
MHartnett65 <Hartnett6590@icloud.com>
2024-03-10 19:09:18 -0600

    Week 8

---
+++ Week 8/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    var images = ["native1.jpg", "native2.jpg", "native3.jpg"];
+    var quotes = [
+        "The Earth does not belong to us: we belong to the Earth.",
+        "Treat the earth well. It was not given to you by your parents, it was loaned to you by your children.",
+        "Listen to the wind, it talks. Listen to the silence, it speaks. Listen to your heart, it knows."
+    ];
+
+    var currentIndex = 0;
+
+    function rotateImages() {
+        currentIndex = (currentIndex + 1) % images.length;
+        $("#container").css("background-image", "url('" + images[currentIndex] + "')");
+    }
+
+    function rotateQuotes() {
+        $("#text").fadeOut(1000, function() {
+            currentIndex = (currentIndex + 1) % quotes.length;
+            $(this).text(quotes[currentIndex]).fadeIn(1000);
+        });
+    }
+
+    setInterval(rotateImages, 5000); // Rotate images every 5 seconds
+    setInterval(rotateQuotes, 8000); // Rotate quotes every 8 seconds
+});
